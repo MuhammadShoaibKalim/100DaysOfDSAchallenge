b35d974cee8e73635b1bc235137681d9b837ad86
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-01-09 07:35:36 +0500

    Add files via upload

---
+++ Day02/Practice Question/first.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=122;
+    int b=13;
+
+    int sum=a+b;
+    cout<<sum;
+
+    int sub=a-b;
+    cout<<sub;
+
+    int division=a/b;
+    cout<<division;
+
+    
+     
+
+
+}

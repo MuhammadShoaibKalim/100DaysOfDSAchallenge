87fae682f57915c0563c7e24355ab509c2f464ca
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-04-07 23:24:27 +0500

    Update prime.cpp

--- Day28/Practice Question/prime.cpp
+++ Day28/Practice Question/prime.cpp
@@ -41,3 +41,36 @@ int main() {
      
     return 0;
 }
+
+
+
+
+/*
+#include <iostream>
+using namespace std;
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;   
+    }
+    for (int i = 2; i<n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    if (isPrime(num)) {
+        cout << num << " is a prime number." << endl;
+    } else {
+        cout << num << " is not a prime number." << endl;
+    }
+     
+    return 0;
+}
+*/

10e84c5dad7a98fe74a149045dc573d362c88cea
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-04-17 23:42:41 +0500

    Commenting  Fibonacci code

--- Day28/Practice Question/index.cpp
+++ Day28/Practice Question/index.cpp
@@ -6,3 +6,31 @@ int main() {
    cout<<" Math in DSA ";
     return 0;
 }
+
+
+/*
+// Function to calculate the nth Fibonacci number using dynamic programming
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+
+    vector<int> fib(n + 1);
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= n; ++i)
+        fib[i] = fib[i - 1] + fib[i - 2];
+
+    return fib[n];
+}
+
+int main() {
+    int n;
+    cout << "Enter the value of n to calculate the nth Fibonacci number: ";
+    cin >> n;
+
+    cout << "The " << n << "th Fibonacci number is: " << fibonacci(n) << endl;
+
+    return 0;
+}
+*/ 

c11c2d8be37d1032c5ac4f7255edd13b6f3925db
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-04-18 23:50:56 +0500

    Update first.cpp

--- Day02/Practice Question/first.cpp
+++ Day02/Practice Question/first.cpp
@@ -19,3 +19,29 @@ int main()
 
 
 }
+
+
+/*
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=122;
+    int b=13;
+
+    int sum=a+b;
+    cout<<sum;
+
+    int sub=a-b;
+    cout<<sub;
+
+    int division=a/b;
+    cout<<division;
+
+    
+     
+
+
+}
+*/ 

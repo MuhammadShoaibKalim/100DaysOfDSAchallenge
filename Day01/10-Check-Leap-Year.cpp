fb3bbe70e85267991ff19a3520d74fc3e798a597
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-05-28 04:25:02 +0500

    adding efficient code in leap year checking

--- Day01/10-Check-Leap-Year.cpp
+++ Day01/10-Check-Leap-Year.cpp
@@ -26,3 +26,27 @@ int main() {
    }
     return 0;
 }
+//Adding more easy and short code
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout << "Enter a year to check if it is a leap year: ";
+    cin >> year;
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
+        cout << year << " is a leap year." << endl;
+    }
+    else
+    {
+        cout << year << " is not a leap year." << endl;
+    }
+
+    return 0;
+}
+
+*/

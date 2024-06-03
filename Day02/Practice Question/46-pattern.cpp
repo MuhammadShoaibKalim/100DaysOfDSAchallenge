62d43e7a6c260bc9378f9350d98940b343f30c99
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-06-03 15:10:38 +0500

    Create 46-pattern.cpp

---
+++ Day02/Practice Question/46-pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Your code goes here
+
+    for (int i = 1; i <= 9; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            if (j >= i || j >= 10 - i)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

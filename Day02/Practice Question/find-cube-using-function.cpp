653cb401a92ba3eda8e562f71238c8cf9384f2d7
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-06-06 04:28:30 +0500

    76-find-cube-using-function.cpp

---
+++ Day02/Practice Question/find-cube-using-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+double cube(int num){
+    return (num*num*num);
+}
+int main() {
+    // Your code goes here
+    int num;
+    double d;
+    cout<<" Enter the number to print cube of number :";
+    cin>>num;
+
+   
+
+    d=cube(num);
+    cout<<d;
+
+    return 0;
+}

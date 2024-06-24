db79680bd7f97ef785ff1c6cec08c39086d3c8eb
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-06-24 10:00:20 +0500

    32-Bank-bankenquery-friend-class.cpp

--- Day29/practice Question/32-Bank-bankenquery-friend-class.cpp
+++ Day29/practice Question/32-Bank-bankenquery-friend-class.cpp
@@ -1,1 +1,31 @@
 
+#include <iostream>
+using namespace std;
+class BankAccount{
+         public:
+         double balance;
+         public:
+          BankAccount(double b=0):balance(b){}
+          void deposit(double amount)
+          {
+            balance+=amount;
+          }
+          friend class BalanceEnquery;
+};
+class BalanceEnquery{
+    public:
+    void display(const BankAccount& ba) const
+    {
+        cout<<" The total balance is : "<<ba.balance<<endl;
+    }
+};
+
+int main() {
+   
+      BankAccount bal(1000);
+      bal.deposit(500);
+
+      BalanceEnquery b;
+      b.display(bal);
+    return 0;
+}

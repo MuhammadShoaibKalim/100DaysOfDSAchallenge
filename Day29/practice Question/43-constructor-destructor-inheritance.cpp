c9c0f1cc7bc9d8958b8abe88772f4ef917ff9834
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-06-25 09:17:36 +0500

    43-constructor-destructor-inheritance.cpp

---
+++ Day29/practice Question/43-constructor-destructor-inheritance.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Base {
+    public:
+          Base(){
+            cout<<"Base class constructor. "<<endl;
+          }
+          ~Base(){
+            cout<<"Base class destructor. "<<endl;
+          }
+};
+
+class Derived : public Base {
+        public:
+        Derived(){
+            cout<<"Derived class constructor. "<<endl;
+        }
+        ~Derived(){
+            cout<<"Derived class destructor. "<<endl;
+        }
+
+};
+int main() {
+        Derived d1;
+        
+    
+    return 0;
+}

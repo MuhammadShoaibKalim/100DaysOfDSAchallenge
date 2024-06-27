a5a185327a60fe708e32a4c31a5b4cd07a77789b
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-06-27 13:59:08 +0500

    63-pointer-object.cpp

---
+++ Day29/practice Question/63-pointer-object.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+public:
+    int length;
+    int breadth;
+
+    Rectangle(int l, int b) : length(l), breadth(b) {}
+
+    int area() {
+        return length * breadth;
+    }
+};
+
+int main() {
+    Rectangle rect(10, 5);
+    
+    Rectangle *p;
+    p = &rect;
+    cout << "Length: " << p->length << endl;
+    cout << "Breadth: " << p->breadth << endl;
+    cout << "Area: " << p->area() << endl;
+    
+
+    return 0;
+}

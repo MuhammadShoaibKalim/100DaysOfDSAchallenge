21dcc218ce2edb7a95f4944ad7839d1eeabc11f3
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-06-27 13:51:28 +0500

    Create 62-pointer-object.cpp

---
+++ Day29/practice Question/62-function-overriding.cpp
@@ -0,0 +1,21 @@
+/*
+Declare Pointer Object
+*/
+
+#include <iostream>
+using namespace std;
+class Parent{
+     public:
+        void print(int i) {
+            cout << "Printing int: " << i << endl;
+        }
+        
+};
+int main() {
+            Parent *p;
+            p = new Parent();
+            p->print(12);
+            p->print(12.5);
+    
+    return 0;
+}

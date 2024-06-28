83acf9add4a3e5eb0715c7bae1c5e7bd22b52ad5
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-06-28 23:40:35 +0500

    Late Binding 66-all-in-one-about-inheritance.cpp

--- Day29/practice Question/66-all-in-one-about-inheritance.cpp
+++ Day29/practice Question/66-all-in-one-about-inheritance.cpp
@@ -223,3 +223,28 @@ int main() {
     return 0;
 }
 
+//Late Binding
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    virtual void display() {
+        cout << "Base class display function" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    void display() override {
+        cout << "Derived class display function" << endl;
+    }
+};
+
+int main() {
+    Base* b;
+    Derived d;
+    b = &d;
+    b->display();
+    return 0;
+}

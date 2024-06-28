5b393e487d0a64329a4293c02d605a2276d4f13a
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-06-28 23:37:58 +0500

    Early Binding 66-all-in-one-about-inheritance.cpp

--- Day29/practice Question/66-all-in-one-about-inheritance.cpp
+++ Day29/practice Question/66-all-in-one-about-inheritance.cpp
@@ -198,3 +198,28 @@ int main() {
 
 //Eary Binding
 
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    void display() {
+        cout << "Base class display function" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    void display() {
+        cout << "Derived class display function" << endl;
+    }
+};
+
+int main() {
+    Base b;
+    Derived d;
+    b.display();
+    d.display();
+    return 0;
+}
+

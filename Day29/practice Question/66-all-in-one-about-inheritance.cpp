acdbb343485b7eac2183413aa946b3d9cbf92f70
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-06-28 23:32:04 +0500

    Update 66-all-in-one-about-inheritance.cpp

--- Day29/practice Question/66-all-in-one-about-inheritance.cpp
+++ Day29/practice Question/66-all-in-one-about-inheritance.cpp
@@ -91,3 +91,39 @@ int main() {
     return 0;
 }
 
+// Hierarchical Inheritance
+
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    void display() {
+        cout << "Base class display function" << endl;
+    }
+};
+
+class Derived1 : public Base {
+public:
+    void show() {
+        cout << "Derived1 class show function" << endl;
+    }
+};
+
+class Derived2 : public Base {
+public:
+    void print() {
+        cout << "Derived2 class print function" << endl;
+    }
+};
+
+int main() {
+    Derived1 d1;
+    Derived2 d2;
+    d1.display();
+    d1.show();
+    d2.display();
+    d2.print();
+    return 0;
+}
+

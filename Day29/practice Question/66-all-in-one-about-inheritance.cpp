2ddce417ac01d9973e164907e1e76226fa074410
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-06-28 23:27:35 +0500

    Update 66-all-in-one-about-inheritance.cpp

--- Day29/practice Question/66-all-in-one-about-inheritance.cpp
+++ Day29/practice Question/66-all-in-one-about-inheritance.cpp
@@ -23,3 +23,37 @@ int main() {
     d.show();
     return 0;
 }
+
+//Multiple inheritance
+
+#include <iostream>
+using namespace std;
+
+class Base1 {
+public:
+    void display() {
+        cout << "Base1 class display function" << endl;
+    }
+};
+
+class Base2 {
+public:
+    void show() {
+        cout << "Base2 class show function" << endl;
+    }
+};
+
+class Derived : public Base1, public Base2 {
+public:
+    void print() {
+        cout << "Derived class print function" << endl;
+    }
+};
+
+int main() {
+    Derived d;
+    d.display();
+    d.show();
+    d.print();
+    return 0;
+}

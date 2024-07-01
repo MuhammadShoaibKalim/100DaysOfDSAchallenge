bd19cff31d1fca0ec0d580f0cacd77bea653995c
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-07-01 11:57:58 +0500

    76-show-template.cpp

---
+++ Day29/practice Question/76-show-template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+template <class T>
+
+T show(T a)
+{
+    cout << a << endl;
+    // return a;
+}
+
+int main()
+{
+    cout << " Here is the value of  :";
+    show(4);
+
+    return 0;
+}

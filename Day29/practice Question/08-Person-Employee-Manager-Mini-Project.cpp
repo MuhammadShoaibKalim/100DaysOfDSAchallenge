5932dc49921cb9783e4f18ee2fd21ef1840dc1e4
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-07-04 14:05:02 +0500

    08-Person-Employee-Manager-Mini-Project.cpp

--- Day29/practice Question/08-Person-Employee-Manager-Mini-Project.cpp
+++ Day29/practice Question/08-Person-Employee-Manager-Mini-Project.cpp
@@ -1,10 +1,4 @@
-/*
-Suppose there are two classes A and B. You want to 
-derive a third class C from these( A and B ) Two 
-classes which concept is required for this and you 
-will implement it through a program.
 
-*/
 
 /*
 Suppose there are two classes A and B. You want to 

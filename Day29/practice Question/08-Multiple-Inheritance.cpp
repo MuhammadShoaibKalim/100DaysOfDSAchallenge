f6c1941bcb090922a7032e564dc2c5c79c05fd37
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-07-04 13:12:34 +0500

    Create 08-Multiple-Inheritance.cpp

---
+++ Day29/practice Question/08-Multiple-Inheritance.cpp
@@ -0,0 +1,7 @@
+/*
+Suppose there are two classes A and B. You want to 
+derive a third class C from these( A and B ) Two 
+classes which concept is required for this and you 
+will implement it through a program.
+
+*/

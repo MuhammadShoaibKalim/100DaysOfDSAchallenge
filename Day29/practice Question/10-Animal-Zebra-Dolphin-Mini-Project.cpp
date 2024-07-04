d13c957422ec19af54cd281026eeaf1c7b257a80
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-07-04 17:01:30 +0500

    10-Animal-Zebra-Dolphin-Mini-Project.cpp

--- Day29/practice Question/10-Animal-Zebra-Dolphin-Mini-Project.cpp
+++ Day29/practice Question/10-Animal-Zebra-Dolphin-Mini-Project.cpp
@@ -1,45 +1,59 @@
 /*
 Write a program with a mother class Animal.
 Inside it defines an age and name and set_value
-function.Write two child classes zebra and dolphin
-show the information of zebra and dolphin age and
-the name. Then two bases object Zebra and Dolphin  
-which write a message telling the age and name give 
-some extra information (e.g place of origin)
+function. Write two child classes zebra and dolphin
+show the information on zebras and dolphins. Then 
+create two bases object Zebra and Dolphin which 
+write a message telling the age and name and give some
+extra information (e.g place
+of origin)
 
 */
 
 #include <iostream>
 using namespace std;
-class Animal {
-    private:
-        int age;
-        string name;
-    public:
-        void set_value(int a, string n) {
-            age = a;
-            name = n;
-        }
-        void show() {
-            cout << "Name: " << name << endl;
-            cout << "Age: " << age << endl;
-        }    
-};
-class Zebra :public Animal{
-    public:
-        void show() {
-            cout << "Zebra is from Africa" << endl;
-        }
-};
-class Dolphin :public Animal{
-    public:
-        void show() {
-            cout << "Dolphin is from Ocean" << endl;
-        }
-};
-int main() {
-      
 
-    
+class Animal {
+private:
+    int age;
+    string name;
+public:
+    void set_value(int a, string n) {
+        age = a;
+        name = n;
+    }
+    void show() const {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+class Zebra : public Animal {
+public:
+    void show_info() const {
+        show();
+        cout << "Zebra is from Africa" << endl;
+    }
+};
+
+class Dolphin : public Animal {
+public:
+    void show_info() const {
+        show();
+        cout << "Dolphin is from the Ocean" << endl;
+    }
+};
+
+int main() {
+    Zebra z;
+    z.set_value(14, "Zebra");
+    z.show_info();
+
+    cout << "--------------------" << endl;
+
+    Dolphin d;
+    d.set_value(20, "Dolphin");
+    d.show_info();
+
     return 0;
 }

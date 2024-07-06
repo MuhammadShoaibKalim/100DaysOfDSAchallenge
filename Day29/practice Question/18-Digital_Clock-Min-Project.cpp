2598028350e17f974230d6ad5d61447fd7516822
Muhammad shoaib kalim <120046632+MuhammadShoaibKalim@users.noreply.github.com>
2024-07-06 15:45:25 +0500

    18-Digital_Clock-Min-Project.cpp

---
+++ Day29/practice Question/18-Digital_Clock-Min-Project.cpp
@@ -0,0 +1,14 @@
+/*
+Write a class name DigitalClock to implement a digital clock in c++. Have 
+a data member function to provide the functionality of a 12 hours clock. 
+User input hours Minutes and Seconds. Digital clock displaying its 
+time in the form of HH: MM: SS am/pm
+*/
+
+#include <iostream>
+using namespace std;
+class DigitalClock{};
+int main()
+{
+  
+} 
